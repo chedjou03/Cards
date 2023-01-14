8916fa1e338a12a49867dbe39c66b0507c4d5bcb
Simplice chedjou <chedjou03@yahoo.fr>
2023-01-14 13:33:24 -0600

    add logic to generate and use correlationId or traceID

--- src/main/java/com/card/controller/CardsController.java
+++ src/main/java/com/card/controller/CardsController.java
@@ -6,11 +6,10 @@ import com.card.model.Customer;
 import com.card.model.Properties;
 import com.card.repository.CardsRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -24,8 +23,11 @@ public class CardsController {
     @Autowired
     private CardsServiceConfig cardsServiceConfig;
 
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
+
     @PostMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+    public List<Cards> getCardDetails(@RequestHeader("ChedjouBank-correlation-id") String correlationId,@RequestBody Customer customer) {
+        logger.debug("ChedjouBank-correlation-id : {}. ", correlationId);
         List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
         if (cards != null) {
             return cards;
